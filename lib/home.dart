9d5c1cb98a0a535a65ef9883b1a0f5c6ead41cbf
saminyeaser <saminyeaser1@gmail.com>
2020-06-03 21:20:24 +0600

    news article design

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news360/helper/data.dart';
 import 'package:news360/model/Category_model.dart';
@@ -32,7 +33,25 @@ class _HomeState extends State<Home> {
         elevation: 0.0,
       ),
       body: Container(
-        
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              height: 60,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index){
+                  return CategoryTile(
+                    imageURL: categories[index].imageURL,
+                    cateoryName: categories[index].CategoryName,
+                  );
+                }
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -42,11 +61,53 @@ class CategoryTile extends StatelessWidget {
   CategoryTile({this.imageURL, this.cateoryName});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          Image.network(imageURL, width: 120, height: 60),
+    return GestureDetector(
+      onTap: (){
 
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 16),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(
+                  imageURL,
+                  width: 120,
+                  height: 60,
+                  fit: BoxFit.cover,)),
+            Container(
+              alignment: Alignment.center,
+              width: 120,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.black26,
+              ),
+              child: Text(
+                cateoryName,
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+class BlogTile extends StatelessWidget {
+  final String imageURL, title, description;
+BlogTile({@required this.imageURL,@required this.title,@required this.description});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Image.network(imageURL),
+          Text(title),
+          Text(description)
         ],
       ),
     );
