b40bf2b11c189d04807bb9a9fe4bbf2fc6d25063
saminyeaser <saminyeaser1@gmail.com>
2020-06-04 03:33:03 +0600

    new news file created for category news

--- lib/category.dart
+++ lib/category.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class CategoryNew extends StatefulWidget {
+class CategoryNews extends StatefulWidget {
   @override
   _CategoryNewState createState() => _CategoryNewState();
 }
 
-class _CategoryNewState extends State<CategoryNew> {
+class _CategoryNewState extends State<CategoryNews> {
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -27,3 +27,29 @@ class News {
     }
   }
 }
+
+class CategoryNews {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews(String category) async{
+    String url = 'http://newsapi.org/v2/top-headlines?category=$category&country=us&category=business&apiKey=7f8b47e89b554e16883411572ad070be';
+
+    var response = await http.get(url);
+    var jsonData = json.decode(response.body);
+    if(jsonData['status'] == 'ok'){
+      jsonData['articles'].forEach((element){
+        if(element['urlToImage'] != null && element['description'] != null){
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              url: element['url'],
+              urlToImage: element['urlToImage'],
+              content: element['context']
+          );
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
