2ffbb7f9c6ee7bcb45ec9f908b587357dbd8b809
saminyeaser <saminyeaser1@gmail.com>
2020-06-04 03:52:57 +0600

    categorynews added

--- lib/category.dart
+++ lib/category.dart
@@ -1,13 +1,80 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news360/helper/news.dart';
+import 'package:news360/model/article_model.dart';
 
 class CategoryNews extends StatefulWidget {
+  final String category;
+  CategoryNews({this.category});
+
   @override
   _CategoryNewState createState() => _CategoryNewState();
 }
 
 class _CategoryNewState extends State<CategoryNews> {
+  List<ArticleModel> articles = new List<ArticleModel>();
+  bool _loading = true;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCategoryNews();
+  }
+  getCategoryNews() async{
+    CategoryNewsClass newsCategoryClass= CategoryNewsClass();
+    await newsCategoryClass.getNews(widget.category);
+    articles = newsCategoryClass.news;
+    _loading = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('News'),
+            Text('360',
+              style: TextStyle(
+                  color: Colors.blue
+              ),)
+          ],
+        ),
+        actions: <Widget>[
+          Opacity(
+            opacity: 0,
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(
+                    Icons.add)),
+          )
+        ],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 16),
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  itemCount: articles.length,
+                  itemBuilder: (context, index){
+                    return BlogTile(
+                      imageURL: articles[index].urlToImage,
+                      title: articles[index].title,
+                      description: articles[index].description,
+                      url: articles[index].url,
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
